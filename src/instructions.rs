6004889d565cbe596c0bc732ebda858c492f4421
Alec Divito <divito.alec@gmail.com>
2019-10-17 11:24:22 -0400

    Completed 3rd tutorial

--- src/instructions.rs
+++ src/instructions.rs
@@ -3,8 +3,34 @@
 /// do something
 #[derive(Debug, PartialEq)]
 pub enum Opcode {
+    /// load a number into the specified register
+    /// (ex. LOAD $0 #500, load 500 into register 0)
+    LOAD,
+    
+    /// Add 2 registers together
+    /// (ex. ADD $0 $1 $2, Add register 0 and 1, store result in register 3)
+    ADD,
+    /// Subtract 2 registers together
+    /// (ex. SUB $0 $1 $2, Subtract register 0 and 1, store result in register 3)
+    SUB,
+    /// Multiply 2 registers together
+    /// (ex. MUT $0 $1 $2, Multiply register 0 and 1, store result in register 3)
+    MUL,
+    /// Divide 2 registers together
+    /// (ex. DIV $0 $1 $2, Divide register 0 and 1, store result in register 3)
+    DIV,
+
+    /// jump stright to a new instruction
+    JMP,
+    /// jump moves forward x amount of instructions relative to the program counter position
+    /// (ex. JMP $0, the value inside of register 0 represents how many positions to move)
+    JMPF,
+    /// jump moves backwards x amount of instructions relative to the program counter position
+    /// (ex. JMP $0, the value inside of register 0 represents how many positions to move)
+    JMPB,
+
     /// halt (stop) the currently running program
-    HLT, 
+    HLT,
     /// Illegal opcode, opcode could not be found
     IGL,
 }
@@ -13,6 +39,14 @@ impl From<u8> for Opcode {
     fn from(v: u8) -> Self {
         match v {
             0 => return Opcode::HLT,
+            1 => return Opcode::LOAD,
+            2 => return Opcode::ADD,
+            3 => return Opcode::SUB,
+            4 => return Opcode::MUL,
+            5 => return Opcode::DIV,
+            7 => return Opcode::JMP,
+            8 => return Opcode::JMPF,
+            9 => return Opcode::JMPB,
             _ => return Opcode::IGL
         }
     }
--- src/vm.rs
+++ src/vm.rs
@@ -8,7 +8,9 @@ pub struct VM {
     /// program counter that tracks what instruction we are currently pointed at
     pc: usize,
     /// Stack of instructions to execute
-    program: Vec<u8>
+    program: Vec<u8>,
+    /// keep extra data that a instruction may output (division)
+    remainder: u32,
 }
 
 impl VM {
@@ -18,28 +20,84 @@ impl VM {
             registers: [0; 32],
             program: vec![],
             pc: 0,
+            remainder: 0,
         }
     }
     
     /// Run the VM and start executing instructions
-    /// This is meant to be the most performance-critical part of our language interpreter
     pub fn run(&mut self) {
-        loop {
-            // If our program counter has exceeded the length of the program itself,
-            // something has gone awry
-            if self.pc >= self.program.len() {
-                println!("Program counter exceeded program instruction stack");
-                break;
+        let mut is_not_done = true;
+        while is_not_done {
+            is_not_done = self.execute_instruction();
+        }
+    }
+
+    /// Execute only one instruction of the VM
+    pub fn run_once(&mut self) {
+        self.execute_instruction();
+    }
+
+    /// Execute the instruction that is currently pointed to on the program stack
+    /// return true if command was executed successfully and program is not finished
+    fn execute_instruction(&mut self) -> bool {
+        // If our program counter has exceeded the length of the program itself,
+        // something has gone awry
+        if self.pc >= self.program.len() {
+            println!("Program counter exceeded program instruction stack");
+            return false;
+        }
+        match self.decode_opcode() {
+            Opcode::LOAD => {
+                let register = self.next_8_bits() as usize;
+                let number = self.next_16_bits() as u16;
+                self.registers[register] = number as i32;
+                true
+            },
+            Opcode::ADD => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 + register2;
+                true
+            },
+            Opcode::SUB => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 - register2;
+                true
+            },
+            Opcode::MUL => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 * register2;
+                true
+            },
+            Opcode::DIV => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 / register2;
+                self.remainder = (register1 % register2) as u32;
+                true
+            },
+
+            Opcode::JMP => {
+                let target = self.registers[self.next_8_bits() as usize];
+                self.pc = target as usize;
+                true
             }
-            match self.decode_opcode() {
-                Opcode::HLT => {
-                    println!("HLT encountered");
-                    return;
-                },
-                _ => {
-                    println!("Unrecognized opcode found! Terminating!");
-                    return;
-                }
+            Opcode::JMPF => {
+                true
+            },
+            Opcode::JMPB => {
+                true
+            },
+
+            Opcode::HLT => {
+                println!("HLT encountered");
+                false
+            },
+            _ => {
+                println!("Unrecognized opcode found! Terminating!");
+                false
             }
         }
     }
@@ -51,12 +109,31 @@ impl VM {
         self.pc += 1;
         return opcode;
     }
+
+    /// Get the next byte off of the program stack
+    fn next_8_bits(&mut self) -> u8 {
+        let result = self.program[self.pc];
+        self.pc += 1;
+        return result;
+    }
+
+    /// Get the next 2 bytes off of the program stack
+    fn next_16_bits(&mut self) -> u16 {
+        // read off 2 bytes from the stack, move the first byte up 8 bits
+        let result = ((self.program[self.pc] as u16) << 8) | self.program[self.pc + 1] as u16;
+        self.pc += 2;
+        return result;
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn get_test_vm() -> VM {
+        VM::new()
+    }
 
     #[test]
     fn test_create_vm() {
@@ -79,6 +156,24 @@ mod tests {
         let test_bytes = vec![200,0,0,0];
         test_vm.program = test_bytes;
         test_vm.run();
+        assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_load_opcode() {
+        let mut test_vm = get_test_vm();
+        // 1, 244 is how to represent 500 using 2 u8s in little endian format
+        test_vm.program = vec![1, 0, 1, 244];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 500);
+    }
+
+    #[test]
+    fn test_jmp_opcode() {
+        let mut test_vm = get_test_vm();
+        test_vm.registers[0] = 1;
+        test_vm.program = vec![7, 0, 0, 0];
+        test_vm.run_once();
         assert_eq!(test_vm.pc, 1);
     }
 }
